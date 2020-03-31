1611a9b483ab7a4847a22ef52e98aff07471117c
Heydar Gerayzade <hgarayzada@gmail.com>
2020-03-31 20:12:41 +0400

    bug fix

--- components/facts/FactsList.js
+++ components/facts/FactsList.js
@@ -1,37 +1,28 @@
 export default class FactsList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      translateX: 0, 
-      activeId: -1 
-    };
+    this.state = { active: [-1, -1], x: 0 };
   }
-  handleMouseOver(e, id) {
+  handleMouseOver(e, row, id) {
     e.persist();
-    let winWidth = window.innerWidth;
-    this.setState(state => ({ 
-      translateX: winWidth > 1024 ? -(e.pageX / winWidth * 50) : 0,
-      activeId: id 
-    }));
+    let x = window.innerWidth > 1024 ? -(e.pageX / window.innerWidth * 50) : 0;
+    this.setState(state => ({  x: x, active: [row, id] }));
   }
   handleMouseLeave(e, clear = false) {
-    this.setState(state => ({
-      translateX: clear ? 0 : state.translateX, 
-      activeId: -1
-    }));
+    this.setState(state => ({ x: clear ? 0 : state.x, active: [-1, -1] }));
   }
   render() {
     let n = this.props.facts.length, count = 0;
     let rows = Math.floor(n/9) * 2 + Math.ceil((n - 9 * Math.floor(n/9)) / 5);
-    let id = this.state.activeId;
+    let { active, x } = this.state;
     return(
-      <div className="facts-strip row" onMouseLeave={(e) => this.handleMouseLeave(e, true)}>
+      <div className="facts-strip row padded" onMouseLeave={(e) => this.handleMouseLeave(e, true)}>
         {Array(rows).fill(0).map((row, i) => (
-        <div className="row padded" style={{transform: `translateX(${this.state.translateX}px)`}} key={i}>
+        <div className="row padded" style={{transform: `translateX(${x}px)`, zIndex: i === active[0] ? 2 : 1 }} key={i}>
           {this.props.facts.slice(count, count += (i % 2 === 0 ? 5 : 4)).map(item => 
           <div className="col-eq-5 col-tab-6 col-mob-4" key={item.id}>
-            <div className={'fact ' + (id === item.id ? 'active' : (id !== -1 ? 'muted' : ''))} 
-              onMouseOver={(e) => this.handleMouseOver(e, item.id)} 
+            <div className={'fact ' + (active[1] === item.id ? 'active' : (active[1] !== -1 ? 'muted' : ''))} 
+              onMouseOver={(e) => this.handleMouseOver(e, i, item.id)} 
               onMouseLeave={(e) => this.handleMouseLeave(e)}
             >
               <img src={item.image} alt={item.title} />
