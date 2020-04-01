b03fc534edaed473b40dfa63c6b73ee98e75fd97
Heydar Gerayzade <hgarayzada@gmail.com>
2020-04-01 15:15:52 +0400

    changed logic

--- components/facts/FactsList.js
+++ components/facts/FactsList.js
@@ -3,12 +3,13 @@ export default class FactsList extends React.Component {
     super(props);
     this.state = { active: [-1, -1], x: 0 };
   }
-  handleMouseOver(e, row, id) {
+  handleMouseOver(e, row, col) {
     e.persist();
     let vw = window.innerWidth;
     let pos = e.pageX / vw;
-    let x = vw > 1024 ? (pos > 0.5 ? -1 : 1) * ((pos < 0.5 ? (0.5 + pos) : pos) * 25) : 0;
-    this.setState(state => ({  x: x, active: [row, id] }));
+    let xLookup = { 0: 30, 1: 15, 2: 0, 3: 15, 4: 30, 5: 22.5, 6: 7.5, 7: 7.5, 8: 22.5 };
+    let x = vw > 1024 ? (pos > 0.5 ? -1 : 1) * xLookup[col % 9] : 0;
+    this.setState(state => ({  x: x, active: [row, col] }));
   }
   handleMouseLeave(e) {
     this.setState(state => ({ x: 0, active: [-1, -1] }));
@@ -18,7 +19,7 @@ export default class FactsList extends React.Component {
     let rows = Math.floor(n/9) * 2 + Math.ceil((n - 9 * Math.floor(n/9)) / 5);
     const { active, x } = this.state;
     const getRowStyles = (i) => ({
-      transform: `translateX(${x !== 0 ? x * (1 + ((i % 2) * 0.2)) : 0}px)`, 
+      transform: `translateX(${(x !== 0 && active[1] % 9 !== 2) ? x * (1 + ((i % 3) * 0.3)) : 0}px)`, 
       zIndex: i === active[0] ? 2 : 1
     });
     return(
