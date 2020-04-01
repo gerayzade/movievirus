79faf9d35a1c4281a0b85d5d2bbc2e767a6528b6
Heydar Gerayzade <hgarayzada@gmail.com>
2020-04-02 03:16:19 +0400

    optimizations

--- pages/_app.js
+++ pages/_app.js
@@ -7,11 +7,37 @@ import '~/assets/scss/style.scss';
 
 const VirusApp = ({ Component, pageProps, store }) => {
   return (
-    <Provider store={store}>
-      <PageTransition timeout={300} classNames="page-transition">
-        <Component {...pageProps} />
-      </PageTransition>
-    </Provider>
+    <>
+      <Provider store={store}>
+        <PageTransition timeout={350} classNames="page-transition">
+          <Component {...pageProps} />
+        </PageTransition>
+      </Provider>
+      <style jsx global>{`
+        .page-transition {
+          position: fixed;
+          z-index: 9;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 200vh;
+          transform: translateY(100vh);
+          background-image: linear-gradient(to bottom, rgba(0,0,0,0) 0%, #000 50%, #000 100%);
+          will-change: transform;
+        }
+        .page-transition-enter .page-transition,
+        .page-transition-exit-active .page-transition {
+          transform: translateY(-100vh);
+          transition: transform .7s;
+        }
+        .page-transition-enter-done .page-transition,
+        .page-transition-enter-active .page-transition,
+        .page-transition-exit .page-transition {
+          transition: transform .7s;
+        }
+        `}
+      </style>
+    </>
   )
 }
 
