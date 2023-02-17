03558c55e13c41376aa9446b0be14aca865ead1b
heydargerayzade <heydar.gerayzade@yellow.systems>
2023-02-17 20:41:23 +0400

    updated page limit

--- src/utils/contentful/index.js
+++ src/utils/contentful/index.js
@@ -17,7 +17,7 @@ const client = contentful.createClient({
   space: CONTENTFUL_SPACE_ID,
 })
 
-const PAGE_LIMIT = 20
+const PAGE_LIMIT = 18
 
 const getTags = async (query = {}) => {
   const response = await client.getTags({
