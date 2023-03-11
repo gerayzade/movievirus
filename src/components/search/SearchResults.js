12cc9939d7d175b467d99b75b4735750ebf02210
heydargerayzade <heydar.gerayzade@yellow.systems>
2023-03-11 04:04:08 +0400

    refactor: search utils

--- src/components/search/SearchResults.js
+++ src/components/search/SearchResults.js
@@ -1,4 +1,3 @@
-import uniq from 'lodash/uniq'
 import {
   Fragment,
   useCallback,
@@ -13,8 +12,10 @@ import {
   selectSearchQuery,
   selectSearchResults,
 } from '~/store/selectors'
-import { MIN_SEARCH_QUERY_LENGTH } from '~/utils/constants'
-import { COLOR_PALETTE } from '~/utils/mappings'
+import {
+  getSearchMessage,
+  getSearchResultsWithHighlights,
+} from '~/utils/search'
 import HtmlContent from '~/components/ui/HtmlContent'
 import Link from '~/components/ui/Link'
 
@@ -24,37 +25,16 @@ const SearchResults = () => {
   const searchQuery = useSelector(selectSearchQuery)
   const searchResults = useSelector(selectSearchResults)
 
-  const searchResultsWithHighlights = searchResults.map((result) => {
-    let title = result.title
-    // Split the search query into an array of words
-    const words = searchQuery
-      .replace(/[^a-zA-Z0-9 ]/g, ' ')
-      .replace(/ +/g, ' ')
-      .trim()
-      .split(' ')
-    // Replace matched keywords with a highlighted version
-    words.forEach((word) => {
-      const queryRegexp = new RegExp(word, 'ig')
-      const matches = uniq(title.match(queryRegexp))
-      matches.forEach((match) => {
-        const matchRegexp = new RegExp(`(^|["\' ])(${match})(?![^<]*>|[^<>]*<\/)`, 'g')
-        title = title.replace(matchRegexp, `$1<span style="background: ${COLOR_PALETTE.RED}">${match}</span>`)
-      })
-    })
-    return {
-      ...result,
-      title,
-    }
+  const searchResultsWithHighlights = getSearchResultsWithHighlights({
+    searchQuery,
+    searchResults,
   })
 
-  let searchMessage
-  if (searchQuery) {
-    if (searchQuery.length < MIN_SEARCH_QUERY_LENGTH) {
-      searchMessage = `Please enter at least ${MIN_SEARCH_QUERY_LENGTH} characters`
-    } else if (!searchLoading && !searchResults.length) {
-      searchMessage = 'No results'
-    }
-  }
+  const searchMessage = getSearchMessage({
+    searchLoading,
+    searchQuery,
+    searchResults,
+  })
 
   const handleClick = useCallback((e) => {
     dispatch(setMenuState(false))
---
+++ src/utils/search.js
@@ -0,0 +1,46 @@
+import uniq from 'lodash/uniq'
+import { MIN_SEARCH_QUERY_LENGTH } from '~/utils/constants'
+import { COLOR_PALETTE } from '~/utils/mappings'
+
+export const getSearchMessage = ({
+  searchQuery,
+  searchResults,
+}) => {
+  let searchMessage
+  if (searchQuery) {
+    if (searchQuery.length < MIN_SEARCH_QUERY_LENGTH) {
+      searchMessage = `Please enter at least ${MIN_SEARCH_QUERY_LENGTH} characters`
+    } else if (!searchLoading && !searchResults.length) {
+      searchMessage = 'No results'
+    }
+  }
+  return searchMessage
+}
+
+export const getSearchResultsWithHighlights = ({
+  searchQuery,
+  searchResults,
+}) => {
+  return searchResults.map((result) => {
+    let title = result.title
+    // Split the search query into an array of words
+    const words = searchQuery
+      .replace(/[^a-zA-Z0-9 ]/g, ' ')
+      .replace(/ +/g, ' ')
+      .trim()
+      .split(' ')
+    // Replace matched keywords with a highlighted version
+    words.forEach((word) => {
+      const queryRegexp = new RegExp(word, 'ig')
+      const matches = uniq(title.match(queryRegexp))
+      matches.forEach((match) => {
+        const matchRegexp = new RegExp(`(^|["\' ])(${match})(?![^<]*>|[^<>]*<\/)`, 'g')
+        title = title.replace(matchRegexp, `$1<span style="background: ${COLOR_PALETTE.RED}">${match}</span>`)
+      })
+    })
+    return {
+      ...result,
+      title,
+    }
+  })
+}
